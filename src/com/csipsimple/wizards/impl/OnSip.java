535f60ab374db5427cff630951490c1011395f81
r3gis.3R@gmail.com <r3gis.3R@gmail.com@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2011-01-18 18:48:25 +0000

    Improve onSip wizard. Thx goes to Eric.
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@583 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- src/com/csipsimple/wizards/impl/OnSip.java
+++ src/com/csipsimple/wizards/impl/OnSip.java
@@ -17,7 +17,11 @@
  */
 package com.csipsimple.wizards.impl;
 
+import java.util.HashMap;
+
+import com.csipsimple.R;
 import com.csipsimple.api.SipProfile;
+import com.csipsimple.utils.PreferencesWrapper;
 
 
 public class OnSip extends AuthorizationImplementation {
@@ -27,6 +31,33 @@ public class OnSip extends AuthorizationImplementation {
 		return "OnSIP";
 	}
 	
+	@Override
+	public void setDefaultParams(PreferencesWrapper prefs) {
+		super.setDefaultParams(prefs);
+		//TODO : when codec per network feature will be there set that for wifi
+		prefs.setCodecPriority("G722/16000/1", "240");
+		prefs.setCodecPriority("PCMU/8000/1", "239");
+		prefs.setCodecPriority("GSM/8000/1", "238");
+		
+		
+	}
+	
+	@Override
+	public void fillLayout(final SipProfile account) {
+		super.fillLayout(account);
+		//Override titles so they are consistent with the how our support docs refer to them
+		accountUsername.setTitle(R.string.w_onsip_username);
+		accountUsername.setDialogTitle(R.string.w_onsip_username_desc);
+	
+		accountAuthorization.setTitle(R.string.w_onsip_authentication_name);
+		accountAuthorization.setDialogTitle(R.string.w_onsip_authentication_name_desc);
+		
+		accountPassword.setTitle(R.string.w_onsip_password);
+		accountPassword.setDialogTitle(R.string.w_onsip_password_desc);
+		
+		accountServer.setTitle(R.string.w_onsip_server);
+		accountServer.setDialogTitle(R.string.w_onsip_server_desc);
+	}
 	
 	public SipProfile buildAccount(SipProfile account) {
 		account = super.buildAccount(account);
@@ -34,5 +65,27 @@ public class OnSip extends AuthorizationImplementation {
 		return account;
 	}
 
+	
+	private static HashMap<String, Integer>SUMMARIES = new  HashMap<String, Integer>(){/**
+		 * 
+		 */
+		private static final long serialVersionUID = -5743705263738203615L;
+
+	{
+		put(DISPLAY_NAME, R.string.w_common_display_name_desc);
+		put(USER_NAME, R.string.w_onsip_username_desc);
+		put(AUTH_NAME, R.string.w_onsip_authentication_name_desc);
+		put(PASSWORD, R.string.w_onsip_password_desc);
+		put(SERVER, R.string.w_onsip_server_desc);
+	}};
+	
+	@Override
+	public String getDefaultFieldSummary(String fieldName) {
+		Integer res = SUMMARIES.get(fieldName);
+		if(res != null) {
+			return parent.getString( res );
+		}
+		return "";
+	}
 	
 }
