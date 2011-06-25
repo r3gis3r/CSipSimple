e07fa63090a8f461bde74a30988b3a3571215fae
r3gis.3r@gmail.com <r3gis.3r@gmail.com@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2011-06-25 14:27:33 +0000

    Fixes issue 1103. Add voip.ms wizard - with server list
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@955 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- src/com/csipsimple/wizards/impl/VoipMS.java
+++ src/com/csipsimple/wizards/impl/VoipMS.java
@@ -17,8 +17,16 @@
  */
 package com.csipsimple.wizards.impl;
 
+import java.util.SortedMap;
+import java.util.TreeMap;
 
-public class VoipMS extends AlternateServerImplementation {
+import android.preference.ListPreference;
+
+import com.csipsimple.R;
+import com.csipsimple.api.SipProfile;
+
+
+public class VoipMS extends SimpleImplementation {
 	
 	
 	@Override
@@ -26,5 +34,95 @@ public class VoipMS extends AlternateServerImplementation {
 		return "VoIP.ms";
 	}
 	
+	ListPreference sipServer; 
+	static SortedMap<String, String[]> providers = new TreeMap<String, String[]>(){
+		private static final long serialVersionUID = -2561302247222706262L;
+	{
+		put("Atlanta, GA", new String[] {"atlanta.voip.ms"});
+		put("Chicago, IL", new String[] {"chicago.voip.ms"});
+		put("Dallas, TX", new String[] {"dallas.voip.ms"});
+		put("Houston, TX", new String[] {"houston.voip.ms"});
+		put("Los Angeles, CA", new String[] {"losangeles.voip.ms"});
+		put("New York, NY", new String[] {"newyork.voip.ms"});
+		put("Seattle, WA", new String[] {"seattle.voip.ms"});
+		put("Tampa, FL", new String[] {"tampa.voip.ms"});
+		put("Montreal 2,QC", new String[] {"montreal2.voip.ms"});
+		put("Toronto 2, ON", new String[] {"toronto2.voip.ms"});
+		put("Montreal,QC", new String[] {"montreal.voip.ms"});
+		put("Toronto, ON", new String[] {"toronto.voip.ms"});
+		put("London, UK", new String[] {"london.voip.ms"});
+	}
+	};
+
+	private static final String PROVIDER_LIST_KEY = "provider_list";
 	
+	@Override
+	public void fillLayout(final SipProfile account) {
+		super.fillLayout(account);
+		
+		
+		boolean recycle = true;
+		sipServer = (ListPreference) parent.findPreference(PROVIDER_LIST_KEY);
+		if(sipServer == null) {
+			sipServer = new ListPreference(parent);
+			sipServer.setKey(PROVIDER_LIST_KEY);
+			recycle = false;
+		}
+		
+		CharSequence[] e = new CharSequence[providers.size()];
+		CharSequence[] v = new CharSequence[providers.size()];
+        int i = 0;
+        for(String pv : providers.keySet()) {
+        	e[i] = pv;
+        	v[i] = ((String[]) providers.get(pv))[0];
+        	i++;
+        }
+		
+		sipServer.setEntries(e);
+		sipServer.setEntryValues(v);
+		sipServer.setDialogTitle(R.string.w_common_server);
+		sipServer.setTitle(R.string.w_common_server);
+		sipServer.setDefaultValue("atlanta.voip.ms");
+		
+        String domain = account.reg_uri;
+        if( domain != null ) {
+	        for(CharSequence state : v) {
+	        	String currentComp = "sip:"+state;
+	        	if( currentComp.equalsIgnoreCase(domain) ) {
+	        		sipServer.setValue((String) state);
+	        		break;
+	        	}
+	        }
+        }
+        
+        if(!recycle) {
+            parent.getPreferenceScreen().addPreference(sipServer);
+    	}
+   }
+
+
+	protected String getDomain() {
+		String provider = sipServer.getValue();
+		if(provider != null) {
+			return provider;
+		}
+		return "";
+	}
+	
+	@Override
+	public void updateDescriptions() {
+		super.updateDescriptions();
+		setStringFieldSummary(PROVIDER_LIST_KEY);
+	}
+
+	@Override
+	public String getDefaultFieldSummary(String fieldName) {
+		if(fieldName == PROVIDER_LIST_KEY) {
+			if(sipServer != null) {
+				return sipServer.getEntry().toString();
+			}
+		}
+		
+		return super.getDefaultFieldSummary(fieldName);
+	}
 }
