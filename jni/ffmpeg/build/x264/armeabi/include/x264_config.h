0b6289c7ee40c60cc36102ce3550ee29f9cab721
r3gis.3r@gmail.com <r3gis.3r@gmail.com@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2011-11-14 12:22:22 +0000

    Remove binary from version control
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@1103 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- jni/ffmpeg/build/x264/armeabi/include/x264_config.h
+++
@@ -1,6 +0,0 @@
-#define X264_BIT_DEPTH     8
-#define X264_GPL           1
-#define X264_INTERLACED    1
-#define X264_CHROMA_FORMAT 0
-#define X264_VERSION " r2106 07efeb4"
-#define X264_POINTVER "0.119.2106 07efeb4"
