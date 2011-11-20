d3ad0f89c708fc81282fa3da68ea90319f806a94
r3gis.3r@gmail.com <r3gis.3r@gmail.com@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2011-11-20 08:09:02 +0000

    Dispatch crypto libs outside of csipsimple builds
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@1128 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- dispatch_shared_libs.sh
+++ dispatch_shared_libs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 move_generic_lib() {
+	echo -n "Moving $1.so to $2 project ... "
 	libs_files=$(ls libs/*/${1}.so 2> /dev/null | wc -l)
 	if [ "$libs_files" != "0" ]; then
 		for lib_folder in libs/*; do
@@ -14,19 +15,11 @@ move_generic_lib() {
 }
 
 move_lib() {
-	echo -n "Moving $1 to $2 project ... "
-	libs_files=$(ls libs/*/libpj_${1}_codec.so 2> /dev/null | wc -l)
-	if [ "$libs_files" != "0" ]; then
-		for lib_folder in libs/*; do
-			mkdir -p ../CSipSimpleCodec${2}/${lib_folder};
-			mv ${lib_folder}/libpj_${1}_codec.so ../CSipSimpleCodec${2}/${lib_folder}/libpj_${1}_codec.so;
-		done
-		echo "[OK]";
-	else
-		echo "[KO] - lib not built"
-	fi
+	move_generic_lib libpj_${1}_codec CSipSimpleCodec${2}
 }
 
 move_lib "g7221" "G221"
 move_lib "codec2" "Codec2"
 move_lib "silk" "Silk"
+move_generic_lib "libcrypto" "CSipSimpleCrypto"
+move_generic_lib "libssl" "CSipSimpleCrypto"
