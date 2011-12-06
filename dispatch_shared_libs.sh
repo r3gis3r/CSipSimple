732d2093e9a95f4a517a2095b4e869dc2b0cb583
r3gis.3r@gmail.com <r3gis.3r@gmail.com@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2011-12-06 21:00:59 +0000

    Fixes issue 1414. Only try to copy if it's a folder.
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@1156 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- dispatch_shared_libs.sh
+++ dispatch_shared_libs.sh
@@ -5,8 +5,10 @@ move_generic_lib() {
 	libs_files=$(ls libs/*/${1}.so 2> /dev/null | wc -l)
 	if [ "$libs_files" != "0" ]; then
 		for lib_folder in libs/*; do
-			mkdir -p ../${2}/${lib_folder};
-			mv ${lib_folder}/${1}.so ../${2}/${lib_folder}/${1}.so;
+			if [ -d ${lib_folder} ]; then
+				mkdir -p ../${2}/${lib_folder};
+				mv ${lib_folder}/${1}.so ../${2}/${lib_folder}/${1}.so;
+			fi
 		done
 		echo "[OK]";
 	else
