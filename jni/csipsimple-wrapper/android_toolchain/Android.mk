b52c397ee5b9399f4e76852f5210f5bcd0269514
r3gis3r <r3gis3r@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2012-04-09 08:31:16 +0000

    Add a way to build in full openssl embedded mode as a commented option for now.
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@1376 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- jni/csipsimple-wrapper/android_toolchain/Android.mk
+++ jni/csipsimple-wrapper/android_toolchain/Android.mk
@@ -94,8 +94,16 @@ endif
 
 ifeq ($(MY_USE_TLS),1)
 	LOCAL_STATIC_LIBRARIES += zrtp4pj
+ifeq (1,1)
+# Normal mainstream users mode
 	LOCAL_STATIC_LIBRARIES += crypto_ec_static
 	LOCAL_SHARED_LIBRARIES += libssl libcrypto
+else
+# This is to do builds with full openssl built in.
+# Not use unless you know what you do and create a ssl_static target in openssl lib
+	LOCAL_STATIC_LIBRARIES += ssl_static crypto_static
+	LOCAL_LDLIBS += -lz
+endif
 endif
 
 
