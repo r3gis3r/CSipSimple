12ad9af45d48e811788fa2dae9129015dc1ab549
r3gis3r <r3gis3r@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2012-04-16 19:59:53 +0000

    Auto add filters for mobile4u
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@1397 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- src/com/csipsimple/wizards/impl/Mobile4U.java
+++ src/com/csipsimple/wizards/impl/Mobile4U.java
@@ -26,6 +26,11 @@ import android.text.InputType;
 
 import com.csipsimple.R;
 import com.csipsimple.api.SipProfile;
+import com.csipsimple.models.Filter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class Mobile4U extends SimpleImplementation {
 	
@@ -65,4 +70,29 @@ public class Mobile4U extends SimpleImplementation {
 		return acc;
 	}
 	*/
+	@Override
+	public List<Filter> getDefaultFilters(SipProfile acc) {
+        // Filter1: Rewrite >> Starts with "+" >> Replace match by "00"
+        ArrayList<Filter> filters = new ArrayList<Filter>();
+
+        Filter f = new Filter();
+        f.account = (int) acc.id;
+        f.action = Filter.ACTION_REPLACE;
+        f.matchPattern = "^" + Pattern.quote("+") + "(.*)$";
+        f.replacePattern = "00$1";
+        f.matchType = Filter.MATCHER_STARTS;
+        filters.add(f);
+
+        // Filter2: Rewrite >> Starts with "06" >> Replace match by "0036"
+        f = new Filter();
+        f.account = (int) acc.id;
+        f.action = Filter.ACTION_REPLACE;
+        f.matchPattern = "^" + Pattern.quote("06") + "(.*)$";
+        f.replacePattern = "0036$1";
+        f.matchType = Filter.MATCHER_STARTS;
+        filters.add(f);
+
+        return filters;
+	}
+	
 }
