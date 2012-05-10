6f48827d876634b38ad8504f15e6da2bd4cb52cd
r3gis3r <r3gis3r@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2012-05-10 14:57:28 +0000

    Use optimized SILK build per target
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@1475 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- jni/silk/android_toolchain/Android.mk
+++ jni/silk/android_toolchain/Android.mk
@@ -7,8 +7,23 @@ ifeq ($(MY_USE_SILK),1)
 ### Glue for pjsip codec ###
 include $(CLEAR_VARS)
 LOCAL_MODULE := pj_silk_codec
-
+ifeq ($(TARGET_ARCH_ABI),armeabi)
 SILK_PATH := $(LOCAL_PATH)/../sources/SILK_SDK_SRC_FIX_v1.0.8/
+else
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+SILK_PATH := $(LOCAL_PATH)/../sources/SILK_SDK_SRC_FLP_v1.0.8/
+else
+ifeq ($(TARGET_ARCH_ABI),mips)
+SILK_PATH := $(LOCAL_PATH)/../sources/SILK_SDK_SRC_MIPS_v1.0.8/
+else
+ifeq ($(TARGET_ARCH_ABI),x86)
+SILK_PATH := $(LOCAL_PATH)/../sources/SILK_SDK_SRC_FLP_v1.0.8/
+endif
+endif
+endif
+endif
+
+
 PJ_SILK_PATH := $(LOCAL_PATH)/../pj_sources/
 
 # pj
@@ -20,7 +35,13 @@ LOCAL_C_INCLUDES += $(PJ_DIR)/pjlib/include/ \
 # silk
 LOCAL_C_INCLUDES += $(SILK_PATH)/interface
 SILK_FILES := $(wildcard $(SILK_PATH)/src/*.c)
+ifeq ($(TARGET_ARCH_ABI),mips)
+exclude := %SKP_Silk_warped_autocorrelation_FIX.c
+SILK_FILES := $(filter-out $(exclude),$(SILK_FILES))
+endif
+SILK_FILES_S = $(wildcard $(SILK_PATH)/src/*.S)
 LOCAL_SRC_FILES += $(SILK_FILES:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES += $(SILK_FILES_S:$(LOCAL_PATH)/%=%)
 # self
 LOCAL_C_INCLUDES += $(PJ_SILK_PATH)
 LOCAL_SRC_FILES += ../pj_sources/silk.c
