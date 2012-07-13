d52f7ae08380ed5d91b5f06f96f1a3a4de27104f
r3gis3r <r3gis3r@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2012-07-13 09:20:34 +0000

    Fixes issue 1824. Add a choice for sip server domain.
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@1688 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- src/com/csipsimple/wizards/impl/Ovh.java
+++ src/com/csipsimple/wizards/impl/Ovh.java
@@ -21,23 +21,45 @@
 
 package com.csipsimple.wizards.impl;
 
+import android.preference.ListPreference;
 import android.text.InputType;
 
 import com.csipsimple.R;
 import com.csipsimple.api.SipProfile;
 
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class Ovh extends SimpleImplementation {
 	
-	@Override
-	protected String getDomain() {
-		return "sip.ovh.net";
-	}
 	
 	@Override
 	protected String getDefaultName() {
 		return "Ovh";
 	}
+	
+    ListPreference sipServer; 
+    static SortedMap<String, String> providers = new TreeMap<String, String>(){
+        private static final long serialVersionUID = 1511610509440960269L;
 
+    {
+        put("sip.ovh.fr", "sip.ovh.fr");
+        put("sip.ovh.net", "sip.ovh.net");
+    }
+    };
+
+    private static final String PROVIDER_LIST_KEY = "provider_list";
+    
+
+
+    protected String getDomain() {
+        String provider = sipServer.getValue();
+        if(provider != null) {
+            return provider;
+        }
+        return "";
+    }
+    
 	
 	//Customization
 	@Override
@@ -48,13 +70,67 @@ public class Ovh extends SimpleImplementation {
 		accountUsername.setDialogTitle(R.string.w_common_phone_number);
 		accountUsername.getEditText().setInputType(InputType.TYPE_CLASS_PHONE);
 		
+		
+		// Server selection
+
+        boolean recycle = true;
+        sipServer = (ListPreference) findPreference(PROVIDER_LIST_KEY);
+        if(sipServer == null) {
+            sipServer = new ListPreference(parent);
+            sipServer.setKey(PROVIDER_LIST_KEY);
+            recycle = false;
+        }
+        
+        CharSequence[] e = new CharSequence[providers.size()];
+        CharSequence[] v = new CharSequence[providers.size()];
+        int i = 0;
+        for(String pv : providers.keySet()) {
+            e[i] = pv;
+            v[i] = providers.get(pv);
+            i++;
+        }
+        
+        sipServer.setEntries(e);
+        sipServer.setEntryValues(v);
+        sipServer.setDialogTitle(R.string.w_common_server);
+        sipServer.setTitle(R.string.w_common_server);
+        sipServer.setDefaultValue("sip.ovh.fr");
+
+        if(!recycle) {
+            addPreference(sipServer);
+        }
+        
+        String domain = account.reg_uri;
+        if( domain != null ) {
+            for(CharSequence state : v) {
+                String currentComp = "sip:" + state;
+                if( currentComp.equalsIgnoreCase(domain) ) {
+                    sipServer.setValue(state.toString());
+                    break;
+                }
+            }
+        }
+		
 	}
-	@Override
-	public String getDefaultFieldSummary(String fieldName) {
-		if(fieldName.equals(USER_NAME)) {
-			return parent.getString(R.string.w_ovh_phone_number_desc);
-		}
-		return super.getDefaultFieldSummary(fieldName);
-	}
+
+    @Override
+    public void updateDescriptions() {
+        super.updateDescriptions();
+        setStringFieldSummary(PROVIDER_LIST_KEY);
+    }
+
+    @Override
+    public String getDefaultFieldSummary(String fieldName) {
+        if(fieldName.equals(USER_NAME)) {
+            return parent.getString(R.string.w_ovh_phone_number_desc);
+        }else if(fieldName == PROVIDER_LIST_KEY) {
+            if(sipServer != null) {
+                return sipServer.getEntry().toString();
+            }
+        }
+        
+        return super.getDefaultFieldSummary(fieldName);
+    }
+    
 	
 }
