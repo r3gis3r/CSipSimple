a640835fb92b6b7943df8b2bbebe488b78b5daab
r3gis3r <r3gis3r@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2012-07-24 08:45:22 +0000

    Allow to show password only when entering it.
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@1728 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- src/com/csipsimple/widgets/PasswordPreference.java
+++ src/com/csipsimple/widgets/PasswordPreference.java
@@ -21,28 +21,30 @@
 
 package com.csipsimple.widgets;
 
+import android.content.Context;
+import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
 import com.csipsimple.R;
 import com.csipsimple.utils.Compatibility;
 import com.csipsimple.utils.Log;
 
-import android.content.Context;
-import android.preference.EditTextPreference;
-import android.provider.Settings;
-import android.text.InputType;
-import android.util.AttributeSet;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-import android.view.View.OnClickListener;
-import android.widget.CheckBox;
-
-public class PasswordPreference extends EditTextPreference implements OnClickListener {
+public class PasswordPreference extends EditTextPreference implements OnClickListener, TextWatcher {
 
 	private static final String THIS_FILE = "PasswordPreference";
 	private CheckBox showPwdCheckbox;
-    private boolean canShowPassword;
 
-
+	private boolean canShowPassword = false;
 
 	public PasswordPreference(Context context) {
 		this(context, null);
@@ -50,27 +52,26 @@ public class PasswordPreference extends EditTextPreference implements OnClickLis
 
 	public PasswordPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		canShowPassword =  (Settings.System.getInt(getContext().getContentResolver(), Settings.System.TEXT_SHOW_PASSWORD, 1) == 1); 
-		
-		showPwdCheckbox = new CheckBox(context);
-		showPwdCheckbox.setText(R.string.show_password);
-		showPwdCheckbox.setOnClickListener(this);
 	}
 	
-	
+	@Override
+	protected void onAddEditTextToDialogView(View dialogView, EditText editText) {
+	    super.onAddEditTextToDialogView(dialogView, editText);
+	    editText.addTextChangedListener(this);
+	}
 	
 	@Override
 	protected void onBindDialogView(View view) {
 		super.onBindDialogView(view);
-		if(!canShowPassword) {
-		    return;
-		}
 		try {
-			CheckBox checkbox = showPwdCheckbox;
-			ViewParent oldParent = checkbox.getParent();
+			showPwdCheckbox = new CheckBox(getContext());
+	        showPwdCheckbox.setText(R.string.show_password);
+	        showPwdCheckbox.setOnClickListener(this);
+	        updateCanShowPassword();
+			ViewParent oldParent = showPwdCheckbox.getParent();
 			if (oldParent != view) {
 				if (oldParent != null) {
-					((ViewGroup) oldParent).removeView(checkbox);
+					((ViewGroup) oldParent).removeView(showPwdCheckbox);
 				}
 			}
 			
@@ -79,7 +80,7 @@ public class PasswordPreference extends EditTextPreference implements OnClickLis
 				container = (ViewGroup) container.getChildAt(0);
 			}
 			if (container != null) {
-				container.addView(checkbox, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+				container.addView(showPwdCheckbox, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 			}
 		}catch(Exception e) {
 			// Just do nothing in case weird ROM in use
@@ -89,10 +90,48 @@ public class PasswordPreference extends EditTextPreference implements OnClickLis
 
 	@Override
 	public void onClick(View view) {
+	    if(!canShowPassword) {
+	        // Even if not shown, be very very sure we never come here
+	        return;
+	    }
 		getEditText().setInputType(
 				InputType.TYPE_CLASS_TEXT | (((CheckBox) view).isChecked() ? 
 				InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD :
 				InputType.TYPE_TEXT_VARIATION_PASSWORD));
 	}
+	@Override
+	public void setText(String text) {
+	    super.setText(text);
+	    setCanShowPassword(TextUtils.isEmpty(text));
+	}
+
+	private void updateCanShowPassword() {
+	    if(showPwdCheckbox != null) {
+	        showPwdCheckbox.setVisibility(canShowPassword ? View.VISIBLE : View.GONE);
+	    }
+	}
+	
+	private void setCanShowPassword(boolean canShow) {
+        canShowPassword = canShow;
+        updateCanShowPassword();
+	}
+	
+    @Override
+    public void afterTextChanged(Editable s) {
+        if(s.length() == 0) {
+            setCanShowPassword(true);
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        // Nothing to do
+        
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        // Nothing to do
+    }
 	
 }
