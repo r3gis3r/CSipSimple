e06d15b798c46149d0f5c8962b8ce5359d8f9035
r3gis3r <r3gis3r@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2012-10-18 22:59:17 +0000

    Fixes issue 2023. One of latest webrtc upgrade broke the libyuv misusage flow by propagating problem in other class.
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@1971 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- jni/third_party/android_toolchain/libyuv/Android.mk
+++ jni/third_party/android_toolchain/libyuv/Android.mk
@@ -9,30 +9,33 @@ LOCAL_CFLAGS := $(MY_PJSIP_FLAGS)
 LOCAL_CPP_EXTENSION := .cc
 
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/include $(LOCAL_PATH)/source
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
 
-LOCAL_SRC_FILES := source/compare.cc \
-	source/convert.cc \
-	source/convert_argb.cc \
-	source/convert_from.cc \
-	source/cpu_id.cc \
-	source/format_conversion.cc \
-	source/planar_functions.cc \
-	source/rotate_argb.cc \
-	source/rotate.cc \
-	source/row_common.cc \
-	source/scale.cc \
-	source/scale_argb.cc \
-	source/video_common.cc 
-	
-	
+LOCAL_SRC_FILES := \
+    source/compare.cc \
+    source/convert.cc \
+    source/convert_from.cc \
+    source/convert_from_argb.cc \
+    source/convert_argb.cc \
+    source/cpu_id.cc \
+    source/format_conversion.cc \
+    source/planar_functions.cc \
+    source/rotate.cc \
+    source/rotate_argb.cc \
+    source/row_common.cc \
+    source/row_posix.cc \
+    source/scale.cc \
+    source/scale_argb.cc \
+    source/video_common.cc
+
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-	LOCAL_ARM_NEON  := true
-	LOCAL_SRC_FILES += source/row_neon.cc.neon source/rotate_neon.cc.neon
-else
-	LOCAL_SRC_FILES += source/row_posix.cc
+    LOCAL_CFLAGS += -DLIBYUV_NEON
+    LOCAL_SRC_FILES += \
+        source/compare_neon.cc.neon \
+        source/rotate_neon.cc.neon \
+        source/row_neon.cc.neon \
+        source/scale_neon.cc.neon
 endif
-
 #LOCAL_STATIC_LIBRARIES := cpufeatures
 
 include $(BUILD_STATIC_LIBRARY)
