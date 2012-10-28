7c4619717f45cc4ad9fc8f02172ef85d353bb5b4
r3gis3r <r3gis3r@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2012-10-28 16:57:20 +0000

    Improves main dialer digit/text show/hide of the soft keyboard.
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@1983 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- src/com/csipsimple/ui/dialpad/DigitsEditText.java
+++ src/com/csipsimple/ui/dialpad/DigitsEditText.java
@@ -31,78 +31,77 @@ import android.view.accessibility.AccessibilityEvent;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
+import com.actionbarsherlock.internal.utils.UtilityWrapper;
 import com.csipsimple.R;
+import com.csipsimple.utils.Log;
+
+import java.lang.reflect.Method;
 
 /**
  * EditText which suppresses IME show up.
  */
 public class DigitsEditText extends EditText {
 
-    private boolean isDigit = true;
+    private static final String THIS_FILE = "DigitsEditText";
+    private Boolean isDigit = null;
     // private int baseInputType = InputType.TYPE_NULL;
+    private Method showSoftInputOnFocus;
 
     public DigitsEditText(Context context) {
         this(context, null);
     }
     
-    public DigitsEditText(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        // baseInputType = getInputType();
-        //setIsDigit(isDigit, false);
-    }
-    
-
     public DigitsEditText(Context context, AttributeSet attrs, int style) {
         this(context, attrs);
     }
     
+    public DigitsEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        try {
+            showSoftInputOnFocus = UtilityWrapper.safelyGetSuperclassMethod(getClass(), "setShowSoftInputOnFocus", boolean.class);
+        }catch(RuntimeException e) {
+            Log.e(THIS_FILE, "Cannot find show soft input on focus method ", e);
+        }
+        setIsDigit(true, false);
+    }
     
     public synchronized void setIsDigit(boolean isDigit, boolean autofocus) {
-        if(this.isDigit != isDigit || autofocus || isDigit) {
+        if(this.isDigit == null || this.isDigit != isDigit) {
             this.isDigit = isDigit;
-            final InputMethodManager imm = ((InputMethodManager) getContext()
-                    .getSystemService(Context.INPUT_METHOD_SERVICE));
-            
+            reflexSetShowSoftInputOnFocus(!isDigit);
             if (isDigit) {
                 setRawInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
                 setTextSize(TypedValue.COMPLEX_UNIT_PX, getContext().getResources().getDimension(R.dimen.dialpad_digits_text_size));
-                if (imm != null && imm.isActive(this)) {
-                    imm.hideSoftInputFromWindow(getApplicationWindowToken(), 0);
-                }
             } else {
                 setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
                         | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
                 setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
-                if (imm != null && autofocus) {
-                    imm.showSoftInput(this, 0);
-                }
             }
         }
-
+        applyKeyboardShowHide(autofocus);
     }
 
+    /*
     // @Override
     public boolean isTextSelectable() {
+        if(isDigit == null) {
+            return false;
+        }
         return !isDigit;
     }
-
-    @Override
-    protected void onSelectionChanged(int selStart, int selEnd) {
-        super.onSelectionChanged(selStart, selEnd);
-        final InputMethodManager imm = ((InputMethodManager) getContext()
-                .getSystemService(Context.INPUT_METHOD_SERVICE));
-        if (imm != null && imm.isActive(this) && isDigit) {
-            imm.hideSoftInputFromWindow(getApplicationWindowToken(), 0);
-        }
-    }
+    */
 
     @Override
     protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
         super.onFocusChanged(focused, direction, previouslyFocusedRect);
-        final InputMethodManager imm = ((InputMethodManager) getContext()
-                .getSystemService(Context.INPUT_METHOD_SERVICE));
-        if (imm != null && imm.isActive(this) && isDigit) {
-            imm.hideSoftInputFromWindow(getApplicationWindowToken(), 0);
+        if(focused) {
+            applyKeyboardShowHide(false);
+        }else {
+            final InputMethodManager imm = ((InputMethodManager) getContext()
+                    .getSystemService(Context.INPUT_METHOD_SERVICE));
+            if(imm != null && imm.isActive(this)) {
+                imm.hideSoftInputFromWindow(getApplicationWindowToken(), 0);
+            }
         }
     }
 
@@ -110,12 +109,34 @@ public class DigitsEditText extends EditText {
     public boolean onTouchEvent(MotionEvent event) {
         final boolean ret = super.onTouchEvent(event);
         // Must be done after super.onTouchEvent()
+        applyKeyboardShowHide(false);
+        return ret;
+    }
+    
+/*
+    @Override
+    public boolean requestFocus(int direction, Rect previouslyFocusedRect) {
+        boolean ret = false;
+        if(!isDigit) {
+            ret = super.requestFocus(direction, previouslyFocusedRect);
+        }
+        applyKeyboardShowHide(false);
+        return ret;
+    }
+*/
+    
+    private void applyKeyboardShowHide(boolean autofocus) {
         final InputMethodManager imm = ((InputMethodManager) getContext()
                 .getSystemService(Context.INPUT_METHOD_SERVICE));
-        if (imm != null && imm.isActive(this) && isDigit) {
-            imm.hideSoftInputFromWindow(getApplicationWindowToken(), 0);
+        if (imm != null) {
+            if(isDigit) {
+                if(imm.isActive(this)) {
+                    imm.hideSoftInputFromWindow(getApplicationWindowToken(), 0);
+                }
+            }else if(autofocus) {
+                imm.showSoftInput(this, 0);
+            }
         }
-        return ret;
     }
 
     @Override
@@ -152,7 +173,12 @@ public class DigitsEditText extends EditText {
         // Here we ensure that we hide the keyboard
         // Since this will be fired when virtual keyboard this will probably
         // blink but for now no better way were found to hide keyboard for sure
-        setIsDigit(isDigit, false);
+        applyKeyboardShowHide(false);
     }
     
+    private void reflexSetShowSoftInputOnFocus(boolean show) {
+        if(showSoftInputOnFocus != null) {
+            UtilityWrapper.safelyInvokeMethod(showSoftInputOnFocus, this, show);
+        }
+    }
 }
