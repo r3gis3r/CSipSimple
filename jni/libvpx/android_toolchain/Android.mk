479675137b5d37244de55cd945f9382217470ead
r3gis3r <r3gis3r@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2012-11-02 23:30:13 +0000

    Fixes libvpx initial build
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@1993 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- jni/libvpx/android_toolchain/Android.mk
+++ jni/libvpx/android_toolchain/Android.mk
@@ -8,7 +8,12 @@ include $(CLEAR_VARS)
 LOCAL_PATH := $(VPX_SRC_PATH)
 CONFIG_DIR := $(VPX_ALL_BUILDS_PATH)/$(TARGET_ARCH_ABI)
 LIBVPX_PATH := $(LOCAL_PATH)/../sources
+_local_mk := $(strip $(wildcard $(CONFIG_DIR)/config.mk))
+_local_patched := $(strip $(wildcard $(TOOLCHAIN_PATH)/../.patched_sources))
+ifdef _local_mk
+ifdef _local_patched
 include $(CONFIG_DIR)/config.mk
 DIST_DIR := $(CONFIG_DIR)/out
 include $(VPX_SRC_PATH)/build/make/Android.mk
-
+endif
+endif
