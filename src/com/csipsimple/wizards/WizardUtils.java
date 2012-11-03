ff62c26aca9b6fa04fc06eaf7e62e9b45fd74da3
r3gis3r <r3gis3r@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2012-11-03 20:52:02 +0000

    Add YouMagic wizard
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@1997 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- src/com/csipsimple/wizards/WizardUtils.java
+++ src/com/csipsimple/wizards/WizardUtils.java
@@ -826,6 +826,10 @@ public class WizardUtils {
                     R.drawable.ic_wizard_fusion, 10, 
                     new Locale[]{Locale.JAPAN}, false, false, 
                     Fusion.class));
+            WIZARDS_DICT.put("YOUMAGIC", new WizardInfo("YOUMAGIC", "YouMagic", 
+                    R.drawable.ic_wizard_youmagic, 30, 
+                    new Locale[]{ locale("ru_RU") }, false, false, 
+                    YouMagic.class));
             
             
             
