b7dc530268b5e0f95dc0addf32772f9a86cc5447
r3gis3r <r3gis.3r@gmail.com>
2014-03-29 19:45:34 +0000

    Update supercel wizard
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk/CSipSimple@2381 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- src/com/csipsimple/wizards/impl/SuperCel.java
+++ src/com/csipsimple/wizards/impl/SuperCel.java
@@ -21,18 +21,106 @@
 
 package com.csipsimple.wizards.impl;
 
+import android.preference.ListPreference;
+
+import com.csipsimple.R;
+import com.csipsimple.api.SipProfile;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 
 public class SuperCel extends SimpleImplementation {
-	
+
+    ListPreference sipServer; 
+    static SortedMap<String, String> providers = new TreeMap<String, String>(){
+    /**
+         * 
+         */
+        private static final long serialVersionUID = 2291298928765427732L;
+
+    {
+        put("SuperCel", "67.222.131.147");
+        put("SuperCel-2", "sip2.supersip.tk");
+        put("SuperCel-3", "sip3.supersip.tk");
+        put("SuperCel-4", "sip4.supersip.tk");
+    }
+    };
+
+    private static final String PROVIDER_LIST_KEY = "provider_list";
 
 	@Override
 	protected String getDomain() {
+        String provider = sipServer.getValue();
+        if(provider != null) {
+            return provider;
+        }
 		return "67.222.131.147";
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.csipsimple.wizards.impl.SimpleImplementation#fillLayout(com.csipsimple.api.SipProfile)
+	 */
+	@Override
+	public void fillLayout(SipProfile account) {
+	    super.fillLayout(account);
+        boolean recycle = true;
+        sipServer = (ListPreference) findPreference(PROVIDER_LIST_KEY);
+        if(sipServer == null) {
+            sipServer = new ListPreference(parent);
+            sipServer.setKey(PROVIDER_LIST_KEY);
+            recycle = false;
+        }
+        
+        CharSequence[] e = new CharSequence[providers.size()];
+        CharSequence[] v = new CharSequence[providers.size()];
+        int i = 0;
+        for(String pv : providers.keySet()) {
+            e[i] = pv;
+            v[i] = providers.get(pv);
+            i++;
+        }
+        
+        sipServer.setEntries(e);
+        sipServer.setEntryValues(v);
+        sipServer.setDialogTitle(R.string.w_common_server);
+        sipServer.setTitle(R.string.w_common_server);
+        sipServer.setDefaultValue("67.222.131.147");
+
+        if(!recycle) {
+            addPreference(sipServer);
+        }
+        
+        String domain = account.reg_uri;
+        if( domain != null ) {
+            for(CharSequence state : v) {
+                String currentComp = "sip:" + state;
+                if( currentComp.equalsIgnoreCase(domain) ) {
+                    sipServer.setValue(state.toString());
+                    break;
+                }
+            }
+        }
 	}
 	
 	@Override
 	protected String getDefaultName() {
 		return "SuperCel";
 	}
-	
+
+    @Override
+    public void updateDescriptions() {
+        super.updateDescriptions();
+        setStringFieldSummary(PROVIDER_LIST_KEY);
+    }
+    @Override
+    public String getDefaultFieldSummary(String fieldName) {
+         if(fieldName == PROVIDER_LIST_KEY) {
+            if(sipServer != null) {
+                return sipServer.getEntry().toString();
+            }
+        }
+        
+        return super.getDefaultFieldSummary(fieldName);
+    }
 }
